a5294e8b44773f8f88e41470a3ac80f4c948428e
Ryan <leachrp@comcast.net>
2022-02-06 19:54:25 -0800

    Update Reader of the Quarter certificate

--- src/components/arcertificate.js
+++ src/components/arcertificate.js
@@ -10,6 +10,9 @@ import {
   Img,
   Signatures,
   HR,
+  OutstandingOuterBorder,
+  OutstandingInnerBorder,
+  ThreeRContainer,
 } from "../styles/certstyles";
 import { determineGrade } from "./constants";
 import Logo from "../styles/images/Logo.png";
@@ -22,30 +25,38 @@ const ArCertificate = ({ students, currentQuarter }) => {
 
     return (
       <CertDiv key={student.id}>
-        <H1>AR Reader of the Quarter</H1>
-        <H4>presented to</H4>
-        <H2>{student.name}</H2>
-        <H3>
-          on this 17th day of November, 2021 for reading {formattedWords} words
-          in the {currentQuarter.toLowerCase()}!
-        </H3>
-        <H3>The most in {determineGrade(student.classroom)}!</H3>
-        <Img
-          style={{ width: "13%", marginTop: "2rem" }}
-          src={Logo}
-          alt="CCS Logo"
-        />
-        <Signatures>
-          <HR></HR>
-          <HR></HR>
-        </Signatures>
-        <P>
-          <q>
-            There is more treasure in books, than in all the pirate's loot on
-            Treasure Island.
-          </q>{" "}
-          - Walt Disney
-        </P>
+        <ThreeRContainer>
+          <OutstandingOuterBorder>
+            <OutstandingInnerBorder>
+              <div style={{ display: "inline" }}>
+                <H1>AR Reader of the Quarter</H1>
+                <H4>presented to</H4>
+                <H2>{student.name}</H2>
+                <H3>
+                  on this 9th day of February, 2022 for reading {formattedWords}{" "}
+                  words in the {currentQuarter.toLowerCase()}!
+                </H3>
+                <H3>The most in {determineGrade(student.classroom)}!</H3>
+                <Img
+                  style={{ width: "13%", marginTop: "2rem" }}
+                  src={Logo}
+                  alt="CCS Logo"
+                />
+                <Signatures>
+                  <HR></HR>
+                  <HR></HR>
+                </Signatures>
+                <P>
+                  <q>
+                    There is more treasure in books, than in all the pirate's
+                    loot on Treasure Island.
+                  </q>{" "}
+                  - Walt Disney
+                </P>
+              </div>
+            </OutstandingInnerBorder>
+          </OutstandingOuterBorder>
+        </ThreeRContainer>
       </CertDiv>
     );
   });
